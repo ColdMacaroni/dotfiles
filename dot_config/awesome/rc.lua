48c38ae490cec26de1c3ef4516415eb6602a4c9f
Architect <squishysparrow7.7@gmail.com>
2023-04-11 18:54:21 +1200

    Copy default theme to base my own off of and add theme picker variable

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -49,6 +49,16 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
+-- Pick your theme, or set to nil for default
+-- Current themes: solit
+local theme = "solit"
+
+if theme then
+    theme = theme .. "/theme.lua"
+else
+    theme = gears.filesystem.get_themes_dir() .. "default/theme.lua"
+end
+
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
