6be7e9b07794500fea6e95c42cec21a102903d79
Sofa <squishysparrow7.7@gmail.com>
2023-04-22 22:01:14 +1200

    Let toggle script use either sink or source

--- dot_config/awesome/executable_toggle-audio-mute.sh
+++ dot_config/awesome/executable_toggle-audio-mute.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env sh
-# Toggles mute and sends a notification about it
-wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+# Toggles mute for either audio (SINK) or mic (SOURCE) and sends a notification about it
 
-
-if [ -z "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep MUTED)" ]; then
-    msg="Audio unmuted"
-else
-    msg="Audio muted"
+if ! [ $# -eq 1 ]; then
+    echo "Bad argument number"
+    exit 1
 fi
 
-notify-send "$msg"
+wpctl set-mute "@DEFAULT_AUDIO_$1@" toggle
+
+if [ "$1" = "SOURCE" ]; then
+    name="Mic"
+elif [ "$1" = "SINK" ]; then
+    name="Audio"
+else
+    echo "What kinda source is that??"
+    exit 2
+fi
+
+
+if [ -z "$(wpctl get-volume @DEFAULT_AUDIO_$1@ | grep MUTED)" ]; then
+    action="unmuted"
+else
+    action="muted"
+fi
+
+notify-send -t 800 "$name $action"
