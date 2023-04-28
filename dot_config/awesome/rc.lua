b6aa3311bae9df0fb513eb36c224dc35baa8e25f
Sofa <squishysparrow7.7@gmail.com>
2023-04-28 12:03:02 +1200

    Move firefox to tag 2 if it's the first of its class

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -605,8 +605,20 @@ awful.rules.rules = {
   --},
 
   -- Set Firefox to always map on the tag named "2" on screen 1.
-  -- { rule = { class = "Firefox" },
-  --   properties = { screen = 1, tag = "2" } },
+  {
+    rule = { class = "firefox" },
+    callback = function(c)
+      -- Probably move to own function
+      for i, client in ipairs(client.get()) do
+        if c.class == client.class and c ~= client then
+          return
+        end
+      end
+
+      local tag = awful.tag.find_by_name(nil, "2")
+      c:move_to_tag(tag)
+    end
+  },
 }
 -- }}}
 
