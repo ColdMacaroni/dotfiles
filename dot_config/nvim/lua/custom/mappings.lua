a73071df3cdfa0e2ae002b3660d5f4f59fd2383f
Sofa <squishysparrow7.7@gmail.com>
2023-06-07 01:41:50 +1200

    Add keymaps for fugitive, harpoon, and treesitter context

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -1,9 +1,55 @@
----@type MappingsTable
+--@type MappingsTable
+
 local M = {}
 
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>gg"] = { ":G<CR>", "Open fugitive", opts = { nowait = true } },
+    -- Harpoon
+    ["<leader>ha"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      "Add harpoon mark",
+      opts = { nowait = true },
+    },
+    ["<leader>hm"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      "Open harpoon menu",
+      opts = { nowait = true },
+    },
+    ["<leader>hn"] = {
+      function()
+        require("harpoon.ui").nav_next()
+      end,
+      "Go to next harpoon file",
+      opts = { nowait = true },
+    },
+    ["<leader>hp"] = {
+      function()
+        require("harpoon.ui").nav_prev()
+      end,
+      "Go to previous harpoon file",
+      opts = { nowait = true },
+    },
+    -- TS Context
+    ["<leader>tt"] = {
+      function()
+        vim.cmd.TSContextToggle()
+      end,
+      "Toggle TreeSitter context",
+      opts = { nowait = true },
+    },
+    ["<leader>tg"] = {
+	    function()
+        require("treesitter-context").go_to_context()
+      end,
+      "Go to TreeSitter context",
+      opts = { nowait = true },
+    },
   },
 }
 
