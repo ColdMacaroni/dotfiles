292503e23d8d4415c4786aa3ec8e084a02cc142e
Sofa <squishysparrow7.7@gmail.com>
2023-06-07 01:40:29 +1200

    Add texlab as a language server

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "jdtls", "phpactor", "sqlls", "cmake", "rust_analyzer", "bashls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "jdtls", "phpactor", "sqlls", "cmake", "rust_analyzer", "bashls", "texlab"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
