63bac32c888a183f3bd1ed658835298c374e190b
Sofa <squishysparrow7.7@gmail.com>
2023-07-19 21:00:40 +1200

    Modify awesome to be able to load custom wibars

---
+++ dot_config/awesome/themes/sotil/wibar.lua
@@ -0,0 +1,18 @@
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+
+-- Widget and layout library
+local wibox = require("wibox")
+
+function create_wibar(s)
+  local M = {
+
+  }
+
+  return M
+end
+
+-- Temp nil return to not mess up my awesome
+return nil
+-- return create_wibar
