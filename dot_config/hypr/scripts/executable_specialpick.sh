522568eadc51d8f5ca5164521ffecf545c5cf94c
Sofa <squishysparrow7.7@gmail.com>
2023-07-30 17:14:35 +1200

    Show the default workspace as (default)

--- dot_config/hypr/scripts/executable_specialpick.sh
+++ dot_config/hypr/scripts/executable_specialpick.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-workspaces=$(hyprctl -j workspaces | jq -r '.[] | select (.id < 0) | .name' | awk -F: '{ print $2 }') #) | sed 's/^$/(default)/')
+workspaces=$(hyprctl -j workspaces | jq -r '.[] | select (.id < 0) | .name' | awk -F: '{ print $2 }' | sort | sed 's/^$/(default)/')
 
-echo "$workspaces" | fuzzel -d --prompt="special:" --log-level=none
+echo "$workspaces" | fuzzel -d --prompt="special:" --log-level=none | sed 's/^(default)$//'
