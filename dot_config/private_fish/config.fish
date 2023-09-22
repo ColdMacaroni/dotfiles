08ef4e0add2511d03156c46430c992e1afc840c8
Sofa <squishysparrow7.7@gmail.com>
2023-09-22 17:06:29 +1200

    Add gopath to env
    It kept making a ~/go folder which i hate very much

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,9 @@ if status is-interactive
   set -Ux EDITOR nvim
   set -Ux VISUAL nvim
   set -Ux XDG_CONFIG_HOME $HOME/.config
+  set -Ux XDG_DATA_HOME $HOME/.local/share
   set -Ux BUNDLE_PATH $HOME/.gems
+  set -g GOPATH $XDG_DATA_HOME/go
 
   set -U tide_character_icon 
   set -U tide_left_prompt_items pwd git jobs character
